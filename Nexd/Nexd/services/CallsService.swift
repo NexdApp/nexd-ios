3014ec93622a3a34d765d0bfaf6d4e79abe46713
Tobias Schröpf <tobias.schroepf@jambit.com>
2020-04-06 02:03:27 +0200

    Download not working :(

--- Nexd/Nexd/services/CallsService.swift
+++ Nexd/Nexd/services/CallsService.swift
@@ -10,8 +10,18 @@ import Foundation
 import NexdClient
 import RxSwift
 
+enum CallsError: Error {
+    case downloadFailed
+}
+
 class CallsService {
     static let shared = CallsService()
+
+    private lazy var urlSession: URLSession = {
+        let config = URLSessionConfiguration.default
+        config.httpAdditionalHeaders = [ "Authorization": "Bearer \(Storage.shared.authorizationToken ?? "-")" ]
+        return URLSession(configuration: config, delegate: nil, delegateQueue: nil)
+    }()
 
     func allCalls() -> Single<[Call]> {
         return CallsAPI.callsControllerCalls()
@@ -19,7 +29,26 @@ class CallsService {
     }
 
     func callFileUrl(sid: String) -> Single<URL> {
-        return CallsAPI.callsControllerGetCallUrl(sid: sid)
-            .asSingle()
+        return Single.create { single -> Disposable in
+            let sid = "CA7fad946f02195f0faa2b6c5f79207429"
+            let url = URL(string: NexdClientAPI.basePath +  "/call/calls/\(sid)/record")!
+
+            let task = self.urlSession.downloadTask(with: url) { localURL, urlResponse, error in
+                guard let localURL = localURL else {
+                    let error = error ?? CallsError.downloadFailed
+                    single(.error(error))
+                    return
+                }
+
+                log.debug("Saved audio file to: \(localURL)")
+                single(.success(localURL))
+            }
+
+            task.resume()
+            return Disposables.create()
+        }
+
+//        return CallsAPI.callsControllerGetCallUrl(sid: sid)
+//            .asSingle()
     }
 }
