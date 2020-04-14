f6ebd6396aa4113e1f9bba72d104e0a04fc6619d
Tobias Schröpf <tobias.schroepf@jambit.com>
2020-04-14 15:49:23 +0200

    Add transcribe info view

--- Nexd/Nexd/ui/helper/transcription/TranscribeInfoView.swift
+++ Nexd/Nexd/ui/helper/transcription/TranscribeInfoView.swift
@@ -9,7 +9,40 @@
 import SwiftUI
 
 struct TranscribeInfoView: View {
+    @ObservedObject var viewModel: ViewModel
+
     var body: some View {
         return Text("Hello world!")
     }
 }
+
+extension TranscribeInfoView {
+    class ViewModel: ObservableObject {
+        private let navigator: ScreenNavigating
+
+        init(navigator: ScreenNavigating) {
+            self.navigator = navigator
+        }
+    }
+}
+
+#if DEBUG
+    struct TranscribeInfoView_Previews: PreviewProvider {
+        static var previews: some View {
+            let viewModel = TranscribeInfoView.ViewModel(navigator: PreviewNavigator())
+            return Group {
+                TranscribeInfoView(viewModel: viewModel)
+                    .environment(\.locale, .init(identifier: "de"))
+
+                TranscribeInfoView(viewModel: viewModel)
+                    .environment(\.colorScheme, .light)
+                    .environment(\.locale, .init(identifier: "en"))
+
+                TranscribeInfoView(viewModel: viewModel)
+                    .environment(\.colorScheme, .dark)
+                    .environment(\.locale, .init(identifier: "en"))
+            }
+            .previewLayout(.sizeThatFits)
+        }
+    }
+#endif
