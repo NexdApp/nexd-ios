00146e402e7c6549a307699afba8df728a0ee0a2
Christian Schmutte <christianschmutte@gmail.com>
2020-04-24 15:29:32 +0200

    Add strike through on tap for ShoppingListItemView

--- Nexd/Nexd/ui/components/ShoppingListItemView.swift
+++ Nexd/Nexd/ui/components/ShoppingListItemView.swift
@@ -11,11 +11,18 @@ import UIKit
 class ShoppingListItemView: CustomView {
     let title = UILabel()
     let amount = UILabel()
-
+    private var doneState: Bool = true
+    let strikeThrough = UIView()
     override func setupView() {
         addSubview(title)
         addSubview(amount)
+        addSubview(strikeThrough)
+        strikeThrough.isHidden = doneState
+        strikeThrough.backgroundColor = .black
         amount.textAlignment = .right
+        self.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.addGestureRecognizer(tapGesture)
     }
 
     override func setupConstraints() {
@@ -29,5 +36,15 @@ class ShoppingListItemView: CustomView {
             make.right.equalToSuperview().offset(-23)
             make.top.bottom.equalToSuperview()
         }
+        strikeThrough.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(-17)
+            make.left.equalToSuperview().offset(17)
+            make.height.equalTo(2)
+            make.centerY.equalTo(self)
+        }
+    }
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        doneState = !doneState
+        strikeThrough.isHidden = doneState
     }
 }
