55d796e35a493c9828fd67bfc9f05bc05ccb71c6
Tobias Schröpf <tobias.schroepf@jambit.com>
2020-06-04 00:10:02 +0200

    WIP

--- Nexd/Nexd/ui/helper/DeliveryConfirmationView.swift
+++ Nexd/Nexd/ui/helper/DeliveryConfirmationView.swift
@@ -18,7 +18,7 @@ struct DeliveryConfirmationView: View {
         let phoneNumber: String
         let address: String
 
-        static func from(helpRequest: HelpRequest) -> Request {
+        static func from(helpRequest: HelpRequest) -> DeliveryConfirmationView.Request {
             Request(requestId: helpRequest.id ?? 0,
                     requester: helpRequest.firstName ?? "-",
                     phoneNumber: helpRequest.phoneNumber ?? "-",
@@ -44,42 +44,34 @@ struct DeliveryConfirmationView: View {
 
                                     NexdUI.Card {
                                         VStack {
-                                            R.string.localizable.delivery_confirmation_phone_number_title.text
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                                .font(R.font.proximaNovaSoftBold.font(size: 18))
-                                                .foregroundColor(R.color.darkListItemTitle.color)
+                                            NexdUI.Texts.cardSectionHeader(text: R.string.localizable.delivery_confirmation_phone_number_title.text)
 
-                                            Text(request.phoneNumber)
+                                            NexdUI.Texts.cardPlaceholderText(text: Text(request.phoneNumber))
                                                 .frame(maxWidth: .infinity, alignment: .trailing)
-                                                .font(R.font.proximaNovaSoftRegular.font(size: 20))
-                                                .foregroundColor(R.color.nexdGreen.color)
 
                                             Divider()
 
-                                            R.string.localizable.delivery_confirmation_address_title.text
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                                .font(R.font.proximaNovaSoftBold.font(size: 18))
-                                                .foregroundColor(R.color.darkListItemTitle.color)
+                                            NexdUI.Texts.cardSectionHeader(text: R.string.localizable.delivery_confirmation_address_title.text)
 
-                                            Text(request.address)
+                                            NexdUI.Texts.cardPlaceholderText(text: Text(request.address))
                                                 .frame(maxWidth: .infinity, alignment: .trailing)
-                                                .font(R.font.proximaNovaSoftRegular.font(size: 20))
-                                                .foregroundColor(R.color.nexdGreen.color)
                                         }
                                     }
-                                    .padding([.leading, .trailing], 12)
+                                    .padding(12)
                                 }
                             }
                         }
-
-                        NexdUI.Buttons.default(text: R.string.localizable.delivery_confirmation_confirm_button_title.text) {
-                            self.viewModel.continueButtonTapped()
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.top, 0)
-                        .padding(.bottom, 40)
                     }
                 }
+
+                Spacer()
+
+                NexdUI.Buttons.default(text: R.string.localizable.delivery_confirmation_confirm_button_title.text) {
+                    self.viewModel.continueButtonTapped()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 0)
+                .padding(.bottom, 40)
             }
             .padding([.leading, .trailing], 20)
             .withBackButton { self.viewModel.backButtonTapped() }
@@ -94,9 +86,9 @@ extension DeliveryConfirmationView {
 
         private var cancellableSet = Set<AnyCancellable>()
 
-        var requests: [Request]? {
-            helpList?.helpRequests.map { helpRequest -> Request in
-                Request.from(helpRequest: helpRequest)
+        var requests: [DeliveryConfirmationView.Request]? {
+            helpList?.helpRequests.map { helpRequest -> DeliveryConfirmationView.Request in
+                DeliveryConfirmationView.Request.from(helpRequest: helpRequest)
             }
         }
 
@@ -136,3 +128,54 @@ extension DeliveryConfirmationView {
         return screen
     }
 }
+
+#if DEBUG
+    struct DeliveryConfirmationView_Previews: PreviewProvider {
+        static var previews: some View {
+            let helperWorkflowState = HelperWorkflowState()
+            helperWorkflowState.helpList = HelpList(id: 1,
+                                                    helpRequestsIds: [1, 2, 3],
+                                                    ownerId: nil,
+                                                    owner: nil,
+                                                    createdAt: Date(),
+                                                    updatedAt: Date(),
+                                                    status: nil,
+                                                    helpRequests: [HelpRequest(firstName: "First",
+                                                                               lastName: "Last",
+                                                                               street: "Street",
+                                                                               number: "Number",
+                                                                               zipCode: "ZIP code",
+                                                                               city: "City",
+                                                                               id: 1,
+                                                                               helpListId: nil,
+                                                                               createdAt: nil,
+                                                                               priority: nil,
+                                                                               additionalRequest: "Additional request",
+                                                                               deliveryComment: "Delivery Comment",
+                                                                               phoneNumber: "+4915112345678",
+                                                                               status: nil,
+                                                                               articles: nil,
+                                                                               requesterId: nil,
+                                                                               requester: nil,
+                                                                               helpList: nil,
+                                                                               callSid: nil)])
+            let viewModel = DeliveryConfirmationView.ViewModel(navigator: PreviewNavigator(), helperWorkflowState: helperWorkflowState, helpListsService: HelpListsService())
+            return Group {
+                DeliveryConfirmationView(viewModel: viewModel)
+                    .background(R.color.nexdGreen.color)
+                    .environment(\.locale, .init(identifier: "de"))
+
+                DeliveryConfirmationView(viewModel: viewModel)
+                    .background(R.color.nexdGreen.color)
+                    .environment(\.colorScheme, .light)
+                    .environment(\.locale, .init(identifier: "en"))
+
+                DeliveryConfirmationView(viewModel: viewModel)
+                    .background(R.color.nexdGreen.color)
+                    .environment(\.colorScheme, .dark)
+                    .environment(\.locale, .init(identifier: "en"))
+            }
+            .previewLayout(.sizeThatFits)
+        }
+    }
+#endif
